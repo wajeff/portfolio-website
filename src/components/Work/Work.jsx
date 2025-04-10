df8f7d6618c53aa464de204c6749c83fd19040ab
Jeff Wang <wa.jeff@outlook.com>
2025-04-10 14:35:39 -0400

    Fixed scroll bug on work__title Added template for works: image, h3, p Added humanity-in-music.png to assets

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,11 +3,27 @@ import styles from './Work.module.css'
 function Work() {
   return (
     <section className = {styles.work}>
-        <h2>Work</h2>
+        <h2 className = {styles.work__title}>Work</h2>
         <section className = {styles.work__container}>
-            <section className={styles["work__works-left"]}>Humanity in Music</section>
-            <section className={styles["work__works-right"]}>Heang's Hair Salon</section>
-            <section className={styles["work__works-left"]}>Candor Lease</section>
+            <section className={styles["work__works-left"]}>
+                <img 
+                    className = {styles["work__works-image"]}
+                    src='../../src/assets/Work/humanity-in-music.png'
+                />
+                <section className = {styles["work__works-description"]}>
+                    <h3 className = {styles["work__works-title"]}>Humanity in Music</h3>
+                    <p className = {["styles.work__works-paragraph"]}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aliquid aut voluptas cumque itaque expedita, esse natus nostrum iste dolorem enim cupiditate dignissimos dolorum velit voluptatum error incidunt harum? Temporibus.
+                    </p>    
+                </section>
+                
+            </section>
+            <section className={styles["work__works-right"]}>
+                
+            </section>
+            <section className={styles["work__works-left"]}>
+                
+            </section>
         </section>
     </section>
   )

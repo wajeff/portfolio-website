0867498bea91b7afa1f5b92be99ac89a64c8a208
Jeff Wang <wa.jeff@outlook.com>
2025-04-10 19:08:04 -0400

    Fixed class naming inconsistency

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -15,7 +15,7 @@ function Work() {
                 />
                 <section className = {styles["work__works-description"]}>
                     <h3 className = {styles["work__works-title"]}>Humanity in Music</h3>
-                    <p className = {["styles.work__works-paragraph"]}>
+                    <p className={styles["work__works-paragraph"]}>
                     Donation website towards Humanity in Music charity.                      </p>    
                 </section>
             </section>
@@ -25,7 +25,7 @@ function Work() {
             <section className={styles["work__works"]}>
                 <section className = {styles["work__works-description"]}>
                     <h3 className = {styles["work__works-title"]}>Candor Lease</h3>
-                    <p className = {["styles.work__works-paragraph"]}>
+                    <p className={styles["work__works-paragraph"]}>
                         Full stack React App that simplies the creation of best practice Toronto rental contracts.
                     </p>    
                 </section>
@@ -44,7 +44,7 @@ function Work() {
                 />
                 <section className = {styles["work__works-description"]}>
                     <h3 className = {styles["work__works-title"]}>Heang's Hair Salon</h3>
-                    <p className = {["styles.work__works-paragraph"]}>
+                    <p className={styles["work__works-paragraph"]}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aliquid aut voluptas cumque itaque sdfsdfsd
                     </p>    
                 </section>

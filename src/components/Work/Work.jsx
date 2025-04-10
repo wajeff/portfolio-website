6a9735593b24977544be2e00813da1b871bb0402
Jeff Wang <wa.jeff@outlook.com>
2025-04-10 16:48:44 -0400

    Added all project pictures Added description for each project Added containres for all projects

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -5,7 +5,10 @@ function Work() {
     <section className = {styles.work}>
         <h2 className = {styles.work__title}>Work.</h2>
         <section className = {styles.work__container}>
-            <section className={styles["work__works-left"]}>
+
+            {/*First Section */}
+
+            <section className={styles["work__works"]}>
                 <img 
                     className = {styles["work__works-image"]}
                     src='../../src/assets/Work/humanity-in-music.png'
@@ -13,17 +16,40 @@ function Work() {
                 <section className = {styles["work__works-description"]}>
                     <h3 className = {styles["work__works-title"]}>Humanity in Music</h3>
                     <p className = {["styles.work__works-paragraph"]}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aliquid aut voluptas cumque itaque expedita, esse natus nostrum iste dolorem enim cupiditate dignissimos dolorum velit voluptatum error incidunt harum? Temporibus.
+                    Donation website towards Humanity in Music charity.                     </p>    
+                </section>
+            </section>
+
+            {/*Second Section */}
+
+            <section className={styles["work__works"]}>
+                <section className = {styles["work__works-description"]}>
+                    <h3 className = {styles["work__works-title"]}>Candor Lease</h3>
+                    <p className = {["styles.work__works-paragraph"]}>
+                        Full stack React App that simplies the creation of best practice Toronto rental contracts.
                     </p>    
                 </section>
-                
+                <img 
+                    className = {styles["work__works-image"]}
+                    src='../../src/assets/Work/candor.png'
+                />
             </section>
-            <section className={styles["work__works-right"]}>
-                
+
+            {/*Third Section */}
+
+            <section className={styles["work__works"]}>
+                <img 
+                    className = {styles["work__works-image"]}
+                    src='../../src/assets/Work/hair.jpg'
+                />
+                <section className = {styles["work__works-description"]}>
+                    <h3 className = {styles["work__works-title"]}>Heang's Hair Salon</h3>
+                    <p className = {["styles.work__works-paragraph"]}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aliquid aut voluptas cumque itaque sdfsdfsd
+                    </p>    
+                </section>
             </section>
-            <section className={styles["work__works-left"]}>
-                
-            </section>
+            
         </section>
     </section>
   )

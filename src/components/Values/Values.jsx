4e2a83f43cf95508551faec43152cbcd6a391971
Jeff Wang <wa.jeff@outlook.com>
2025-04-17 11:43:27 -0400

    Created Values component and css module

---
+++ src/components/Values/Values.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Values.module.css'
+
+const Values = () => {
+  return (
+    <section className={styles.values}>
+        
+    </section>
+  )
+}
+
+export default Values

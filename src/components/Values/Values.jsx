2b512225b9b4a42e62d5b812545ce23aea49128f
Jeff Wang <wa.jeff@outlook.com>
2025-04-22 19:39:39 -0400

    Removed rem changes within module files to prevent conflicts with App.css declaration
    Resized pages to 95% width on mobile instead of 70%
    
    Removed values__title-container for being redundant

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -4,13 +4,10 @@ import styles from './Values.module.css'
 const Values = () => {
   return (
     <section className={styles.values} id ='Values'>
-        <section className={styles[`values__title-container`]}>
           <h2 className={styles.values__title}>Values:</h2>
           <h2 className={styles.values__title}>Minimalist.</h2>
           <h2 className={styles.values__title}>Intuitive.</h2>
           <h2 className={styles.values__title}>Useful.</h2>
-        </section>
-        
         <section className={styles[`values__paragraph-container--right`]}>
           <p className={styles[`values__paragraph`]}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aperiam cum, ullam alias veniam accusantium, impedit debitis eveniet facere deserunt earum? Neque ratione aliquid nobis commodi doloribus vero id recusandae!
